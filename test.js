f698476ed6271af3012819775f4dc7d7dc2320f6
hukudome <koreaitdeveloper@gmail.com>
2024-04-27 17:38:09 -0700

    remove test files

--- test.js
+++
@@ -1,8 +0,0 @@
-const handler = require('./handler.js');
-
-async function testWeatherFunction() {
-  const event = { latitude: '49.282730', longitude: '-123.120735' };
-  const result = await handler.getWeather(event);
-  console.log(result);
-}
-testWeatherFunction();
